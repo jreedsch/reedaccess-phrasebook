f58b499a8406021689cda1c1244265cfd26ab47d
jreedsch <reedsch@gmail.com>
2013-12-29 21:46:10 +0700

    heroku 5

--- phrasebookserver.js
+++ phrasebookserver.js
@@ -1,14 +1,46 @@
-var express = require("express");
+//heroku deployed version
+var express = require('express')
+  , http    = require('http')
+  , path    = require('path')
+  , reload  = require('reload')
+  , phrases = require('./server/api/phrases')
+  , colors  = require('colors')
+
 var logfmt = require("logfmt");
+
 var app = express();
 
+var clientDir = path.join(__dirname, 'client')
+
+app.configure(function() {
+  app.set('port', process.env.PORT || 8081)
+  app.use(express.favicon())
+  app.use(express.logger('dev'))
+  app.use(express.bodyParser()) 
+  app.use(app.router) 
+  app.use(express.static(clientDir)) 
+})
+
 app.use(logfmt.requestLogger());
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+})
+
+var server = http.createServer(app)
+reload(server, app)
 
 app.get('/', function(req, res) {
   res.send('Hello from ReedAccess Phrasebook!');
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+server.listen(app.get('port'), function(){
+  console.log("Phrasebook server is listening in %s on port %d", colors.red(process.env.NODE_ENV), app.get('port'));
 });
+
+
+// return the primary html page
+app.get('/', function(req, res) {
+  res.send("Hello Reedaccess Phrasebook");
+  //res.sendfile(path.join(clientDir, 'index.html'))
+})
